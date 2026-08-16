ba0276be94d5cd957ce76228218eacc30bc8ed10
amrxtgh <152877271+amrxtgh@users.noreply.github.com>
2026-08-17 00:36:00 +0545

    Time: 0 ms (100%), Space: 2.5 MB (19.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,12 +1,13 @@
+use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let n = nums.len();
-        for i in 0..n {
-            for j in (i+1)..n {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut seen: HashMap<i32, usize> = HashMap::new();
+        for (i, &nums) in nums.iter().enumerate() {
+            let complement = target - nums;
+            if let Some(&prev_index) = seen.get(&complement) {
+                return vec![prev_index as i32, i as i32];
             }
+            seen.insert(nums, i);
         }
         vec![]
     }
