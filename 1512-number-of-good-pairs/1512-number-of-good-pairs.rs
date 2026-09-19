419f56ca472c695f009959f8f15553976550c671
amrxtgh <152877271+amrxtgh@users.noreply.github.com>
2026-09-20 01:35:07 +0545

    Time: 0 ms (100%), Space: 2.2 MB (7.41%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.rs
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
-        let mut counts = HashMap::new();
-
+        let mut count = [0; 101];
+        let mut result = 0;
         for num in nums {
-            *counts.entry(num).or_insert(0) += 1;
+            let index = num as usize;
+            result += count[index];
+            count[index] += 1;
         }
-        counts.values().map(|&n| n*(n-1)/2).sum()
+        result
     }
 }
