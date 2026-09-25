cda3df618bc9b0e12066d3f325f0c384a5c081fc
amrxtgh <152877271+amrxtgh@users.noreply.github.com>
2026-09-25 23:55:43 +0545

    Time: 0 ms (100%), Space: 2.2 MB (100%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
@@ -1,7 +1,5 @@
 impl Solution {
     pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
-        let str1 = word1.concat();
-        let str2 = word2.concat();
-        str1 == str2
+        word1.concat() == word2.concat()
     }
 }
